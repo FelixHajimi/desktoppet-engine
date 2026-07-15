8aac0b96e1561c885755009c76c037c4b626aeca
Felix <KLDXYO@outlook.com>
2026-07-15 22:44:29 +0800

    feat(plugin): add activate,deactivate and init functions

--- .src/examples/plugin/Template/enter.py
+++ .src/examples/plugin/Template/enter.py
@@ -1,23 +1,23 @@
 from PySide6 import QtCore, QtGui, QtWidgets
+import os
 
+PATH = os.path.dirname(__file__)
 
 class Template:
     def __init__(self):
         self._autoStart = False
 
-    def enter(
-        self,
-        image: QtGui.QMovie,
-        mainTimer: QtCore.QTimer,
-        physicsTimer: QtCore.QTimer,
-        state: dict,
-        window: QtWidgets.QWidget,
-    ):
+    def init(self, image, mainTimer, physicsTimer, window):
         self._image = image
         self._mainTimer = mainTimer
         self._physicsTimer = physicsTimer
-        self._state = state
         self._window = window
+
+    def activate(self, state):
+        self._state = state
+
+    def deactivate(self):
+        pass
 
     def loadMovie(self, path: str):
         if self._image.fileName() != path:
@@ -26,5 +26,5 @@ class Template:
         self._image.start()
 
 
-menu = {}
+menu = {"_type": "/"}
 pluginName = "模板"
--- .src/examples/plugin/reminder/enter.py
+++ .src/examples/plugin/reminder/enter.py
@@ -1,16 +1,28 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 import random
 
+
 class Template:
     def __init__(self):
         self._autoStart = False
 
-    def enter(self, image, mainTimer, physicsTimer, state, window):
+    def init(self, image, mainTimer, physicsTimer, window):
         self._image = image
         self._mainTimer = mainTimer
         self._physicsTimer = physicsTimer
-        self._state = state
         self._window = window
+
+    def activate(self, state):
+        self._state = state
+
+    def deactivate(self):
+        pass
+
+    def loadMovie(self, path: str):
+        if self._image.fileName() != path:
+            self._image.setFileName(path)
+            self._image.jumpToFrame(0)
+        self._image.start()
 
 
 class Reminder(Template):
@@ -18,37 +30,38 @@ class Reminder(Template):
         super().__init__()
         self._autoStart = True
 
-    def enter(self, image, mainTimer, physicsTimer, state, window):
-        super().enter(image, mainTimer, physicsTimer, state, window)
+    def activate(self, state):
+        super().activate(state)
         print("提醒插件已加载！")
-        
-        timer = QtCore.QTimer()
-        timer.timeout.connect(self.show_reminder)
-        timer.start(2160000)
-        self._state["reminder_timer"] = timer
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.show_reminder)
+        self.timer.start(2160000)
+        state["$reminder"]["reminder_timer"] = self.timer
+
+    def deactivate(self):
+        super().deactivate()
+        del self.timer
 
     def show_reminder(self):
         messages = ["该休息一下了！", "起来走动走动！", "喝口水吧！"]
-        QtWidgets.QMessageBox.information(
-            self._window, 
-            "提醒", 
-            random.choice(messages)
-        )
+        QtWidgets.QMessageBox.information(self._window, "提醒", random.choice(messages))
 
 
 class StopReminder(Template):
-    def enter(self, image, mainTimer, physicsTimer, state, window):
-        super().enter(image, mainTimer, physicsTimer, state, window)
+    def activate(self, state):
+        super().activate(state)
         if "reminder_timer" in state:
-            state["reminder_timer"].stop()
-            state["reminder_timer"].deleteLater()
-            del state["reminder_timer"]
+            state["$reminder"]["reminder_timer"].stop()
+            state["$reminder"]["reminder_timer"].deleteLater()
+            del state["$reminder"]
             print("提醒已关闭")
 
 
 pluginName = "定时提醒"
 
 menu = {
-    "开启提醒": Reminder(),
-    "关闭提醒": StopReminder()
+    "_type": "/",
+    "开启提醒": {"_type": "$", "_activate": Reminder()},
+    "关闭提醒": {"_type": "$", "_activate": StopReminder()},
 }
